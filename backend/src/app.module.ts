6de94c305a780627867ec0cf0920a8ca136997af
Jreng7 <jr.waydev@gmail.com>
2025-03-19 16:37:45 -0300

    criando módulos para aplicação db e projetos

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
+import { DbModule } from './db/db.module'
+import { ProjetoModule } from './projeto/projeto.module';
 
 @Module({
-	imports: [],
+	imports: [DbModule, ProjetoModule],
 	controllers: [AppController],
 })
 export class AppModule {}
