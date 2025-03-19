beb4d8a253f8a7a72f40e0dff11c871b44b47233
Jreng7 <jr.waydev@gmail.com>
2025-03-19 16:38:24 -0300

    último módulo criado "tecnologia"

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { DbModule } from './db/db.module'
 import { ProjetoModule } from './projeto/projeto.module';
+import { TecnologiaModule } from './tecnologia/tecnologia.module';
 
 @Module({
-	imports: [DbModule, ProjetoModule],
+	imports: [DbModule, ProjetoModule, TecnologiaModule],
 	controllers: [AppController],
 })
 export class AppModule {}
